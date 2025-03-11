a88f58d1818cddc3de42e31ce1ec2f1c83bcf440
dashelstroud <142332687+dashelstroud@users.noreply.github.com>
2025-03-11 15:09:05 +0000

    did a lot of the pong project

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -10,13 +10,44 @@ function runProgram(){
   // Constant Variables
   const FRAME_RATE = 60;
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
+
+  const BOARD_WIDTH = $('#board').width()
+  const BOARD_HEIGHT = $('#board').height()
+
+
+
+
   
   // Game Item Objects
+const KEY = {
+  'W':87,
+  'S':83,
+  'up':38,
+  'down':40,
+}
 
+
+  function gameItem(id,speedX, speedY ){
+    var obj = {}
+    obj.id = id
+    obj.posX = parseFloat($(id).css('left'))
+    obj.posY = parseFloat($(id).css('top'))
+    obj.speedX = speedX
+    obj.speedY = speedY
+    obj.width = $(id).width()
+    obj.height = $(id).height()
+
+    return obj
+  }
+
+    var paddleLeft = gameItem('#paddleLeft', 0, 0)
+
+    var paddleRight = gameItem('#paddleRight', 0, 0)
 
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);
+  $(document).on('keyup', handleKeyUp) ;                          // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -27,20 +58,59 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
+    drawGameItem(paddleLeft)
+    moveGameItem(paddleLeft)
 
+    drawGameItem(paddleRight)
+    moveGameItem(paddleRight)
   }
   
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
+  function handleKeyDown(event) {
+    if(event.which === KEY.W){
+      paddleLeft.speedY -= 5
+    }
 
+    if(event.which === KEY.S){
+      paddleLeft.speedY += 5
+    }
+
+    if(event.which === KEY.up){
+      paddleRight.speedY -= 5
+    }
+
+    if(event.which === KEY.down){
+      paddleRight.speedY += 5
+    }
   }
 
+  function handleKeyUp(event) {
+    if(event.which === KEY.W || event.which === KEY.S){
+      paddleLeft.speedY = 0;
+    }
+    
+    if(event.which === KEY.up || event.which === KEY.down){
+      paddleRight.speedY = 0;
+    }
+  }
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
+
+  //movement helpers
+
+function drawGameItem(obj){
+  $(obj.id).css('left', obj.posX)
+  $(obj.id).css('top', obj.posY)
+}
+
+function moveGameItem(obj){
+  obj.posX += obj.speedX;
+  obj.posY += obj.speedY;
+}
+
 
   
   function endGame() {
